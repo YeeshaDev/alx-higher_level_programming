ef4fb160ab4f97a6b674748f91da901ffa41edaf
YeeshaDev <101475738+YeeshaDev@users.noreply.github.com>
2022-08-27 17:14:37 +0100

    inheritance

---
+++ 0x0A-python-inheritance/tests/6-main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('6-base_geometry').BaseGeometry
+
+bg = BaseGeometry()
+
+try:
+    print(bg.area())
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Statu
