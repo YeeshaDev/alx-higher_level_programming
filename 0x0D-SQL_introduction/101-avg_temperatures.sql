0d44cae44ba21e411658aaa95e1a90679c7aaba1
YeeshaDev <101475738+YeeshaDev@users.noreply.github.com>
2022-09-07 10:36:24 +0100

    SQL

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,9 @@
+-- Displays the average temperature (in Fahrenheit) by city ordered by descending temperature.
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+GROUP BY `city`
+ORDER BY `avg_temp` DESC;
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
